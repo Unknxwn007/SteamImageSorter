8d79e240212f6ff88e21e2962cddbd2ad984568c
UNKNØWN <60184894+Unknxwn007@users.noreply.github.com>
2024-05-18 19:11:27 +0200

    1.0
    folder name is the app id currently.

---
+++ SteamImageSorterAuto.py
@@ -0,0 +1,40 @@
+import os
+import shutil
+import configparser
+from pathlib import Path
+
+config = configparser.ConfigParser()
+config.read(Path('config.ini'))
+
+directory = Path(config.get('Settings', 'directory'))
+
+app_ids = set()
+
+for entry in os.scandir(directory):
+    if entry.is_file():
+        file_name = entry.name
+        app_id = file_name.split('_')[0]
+        app_ids.add(app_id)
+
+for app_id in app_ids:
+    if app_id == '0':
+        app_id = 'UNRECOGNIZED'
+    app_id_dir = os.path.join(directory, app_id)
+    if not os.path.exists(app_id_dir):
+        os.makedirs(app_id_dir)
+
+for entry in os.scandir(directory):
+    if entry.is_file():
+        file_name = entry.name
+        app_id = file_name.split('_')[0]
+        if app_id == '0':
+            app_id = 'UNRECOGNIZED'
+        dest_path = os.path.join(directory, app_id, file_name)
+        shutil.move(entry.path, dest_path)
+        print(f'Moved {file_name} to the {app_id} directory.')
+
+for app_id in app_ids:
+    if app_id == '0':
+        app_id = 'UNRECOGNIZED'
+    app_id_dir = os.path.join(directory, app_id)
+    print(f'Moved {len(os.listdir(app_id_dir))} files to the {app_id} directory.')
